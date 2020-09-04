49b82ab46ff78cb6e1c561da4b69ef5cd4be63e0
capafasi <cpfaundes@ug.uchile.cl>
2020-09-03 23:07:20 -0400

    add hashtags

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,7 @@
 class Tweet < ApplicationRecord
-
+  include ActionView::Helpers::UrlHelper
+  
+  before_save :hashtags
   validates :content, presence: true
   
   
@@ -13,7 +15,17 @@ class Tweet < ApplicationRecord
 
   scope :tweets_for_me, ->(user) { where(user_id: user.arr_friend_id)}
 
- 
+  def hashtags
+    new_array = []
+    self.content.split(" ").each do |word|
+      if word.start_with?("#")
+        word_parsed = word.sub '#','%23'
+        word = link_to(word, Rails.application.routes.url_helpers.root_path+"?search="+word_parsed)
+      end
+      new_array.push(word)
+    end 
+    self.content = new_array.join(" ")
+  end
 
 
   def is_liked?(user)
